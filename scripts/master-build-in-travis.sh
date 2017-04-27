7c767678d9e60f051b6cc9419e3245373003addb
Ce Gao <ce.gao@outlook.com>
2017-04-27 23:00:37 +0800

    scripts: Add a comment
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- scripts/master-build-in-travis.sh
+++ scripts/master-build-in-travis.sh
@@ -6,6 +6,7 @@ cd ${ROOT}
 
 ./scripts/build-for-alpine.sh
 docker build -t gaocegege/maintainer .
+# $DOCKER_USERNAME and $DOCKER_PASSWORD are defined in Travis UI.
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push gaocegege/maintainer
 
