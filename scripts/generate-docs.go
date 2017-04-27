ab6705b39dcb6eac1cfcf9e447c050fada2b9522
Ce Gao <ce.gao@outlook.com>
2017-04-27 11:54:29 +0800

    LICENSE: Update license
    Signed-off-by: Ce Gao <ce.gao@outlook.com>

--- scripts/generate-docs.go
+++ scripts/generate-docs.go
@@ -1,4 +1,4 @@
-// Copyright © 2017 Ce Gao <ce.gao@outlook.com>
+// Copyright © 2017 Maintainer Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
